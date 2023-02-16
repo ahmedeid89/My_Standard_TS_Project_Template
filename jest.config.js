e5c76f114ffc712bb3190ff7229023dfa1e3f02e
ahmed_eid <ahmedmohamed89302012@gmail.com>
2023-02-16 16:52:37 +0200

    The 1st commit

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const config = {
+        verbose: true,
+        testEnvironment: "jest-environment-node",
+        transform: {},
+        testMatch: ["**/?(*.)+(spec|test).[j]s?(x)"],
+};
+
+export default config;
