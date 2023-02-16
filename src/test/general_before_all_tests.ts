b4752932f5b02110490f37540d857e9189c596f3
ahmed_eid <ahmedmohamed89302012@gmail.com>
2023-02-16 17:35:51 +0200

    Just a regular commit

--- src/test/general_before_all_tests.ts
+++ src/test/general_before_all_tests.ts
@@ -3,6 +3,5 @@ import { copy_The_Current_Database_To_The_myTmpDir } from "../1_04_utility/tmp_d
 export { before_All_Tests };
 //----------------------------------------------------------------------------------------------------
 async function before_All_Tests() {
-        await copy_The_Current_Database_To_The_myTmpDir();
 }
 //----------------------------------------------------------------------------------------------------
