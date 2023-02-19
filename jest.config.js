6cfab7b6becb3c019b493751448e1a5101e3c2f0
ahmed_eid <ahmedmohamed89302012@gmail.com>
2023-02-19 19:20:39 +0200

    Modified the jest template again

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 const config = {
-        verbose: true,
-        testEnvironment: "jest-environment-node",
-        transform: {},
-        testMatch: ["**/?(*.)+(spec|test).[j]s?(x)"],
+	verbose: true,
+	testEnvironment: "jest-environment-node",
+	testMatch: ["**/?(*.)+(spec|test).[t]s?(x)"],
+	testPathIgnorePatterns: ["node_modules", "coverage"],
+	// you must let jest transform and don't compile by your tsc, because it's very possible that you run the test without compiling first, so the test will run on the old compiled js files.
+	transform: {
+		"^.+\\.ts?$": "ts-jest",
+		"^.+\\.ts?x$": "ts-jest",
+	},
 };
 
 export default config;
